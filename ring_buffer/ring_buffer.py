f091fe4bb0a16fc5039d3a522f7347a714cbe8ca
raberin <rmvaberin@gmail.com>
2020-01-31 08:58:20 -0800

    Partially Completed ring_buffer - Needs some debugging

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,27 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # If capacity < len(storage) add to tail
+        if self.capacity > self.storage.length:
+            # Add to tail
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
+        # If it has reached max capacity replace head with new val
+        else:
+            # Remove oldest aka head and append new node
+            print(self.storage.head)
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
         # TODO: Your code here
-
+        current = self.current
+        while current:
+            list_buffer_contents.append(current.value)
+            current = current.next
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
